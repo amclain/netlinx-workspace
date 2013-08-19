efdb9a2f3e1d10ca059a81d249ee1238f48c6fde
Alex McLain <alex@alexmclain.com>
2013-08-18 23:13:23 -0700

    Typo.

--- lib/netlinx/compile/extension/apw.rb
+++ lib/netlinx/compile/extension/apw.rb
@@ -4,7 +4,7 @@ require 'netlinx/workspace'
 module NetLinx
   module Compile
     module Extension
-      # Instructs netlinx-compile on how to process .axs NetLinx source code files.
+      # Instructs netlinx-compile on how to process .apw NetLinx source code files.
       class APW
         def invoke_compile(**kvargs)
           target = kvargs.fetch(:target, nil)
