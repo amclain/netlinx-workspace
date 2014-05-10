75e9774bd943ae6b86e9a582c37bd953ab1be6e3
Alex McLain <alex@alexmclain.com>
2014-05-09 18:31:12 -0700

    apw_spec passes.

--- spec/compile/extension/apw_spec.rb
+++ spec/compile/extension/apw_spec.rb
@@ -5,14 +5,12 @@ describe NetLinx::Compile::Extension::APW do
   
   subject { NetLinx::Compile::Extension::APW }
   
-  let(:path) { 'test\unit\workspace\extension\apw' }
-  
   
   it { should respond_to :get_handler }
   
   it "handles .apw NetLinx workspace files" do
     subject.get_handler.extensions.should include 'apw'
-    subject.get_handler.handler_class.should be_a NetLinx::Workspace
+    subject.get_handler.handler_class.should eq NetLinx::Workspace
   end
   
 end
