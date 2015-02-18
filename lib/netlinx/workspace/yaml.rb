949946ddf61fbab99d3466d0c7b115c35e69eb4a
Alex McLain <alex@alexmclain.com>
2015-02-18 11:35:05 -0800

    Implemented yaml parsing of multiple systems.

--- lib/netlinx/workspace/yaml.rb
+++ lib/netlinx/workspace/yaml.rb
@@ -20,17 +20,11 @@ module NetLinx
           
           yaml_systems.each do |yaml_system|
             NetLinx::System.new.tap do |system|
-              system.name = yaml_system['name']
-              system.id = yaml_system['id']
-              system.description = yaml_system['description']
+              system.name = yaml_system['name'] if yaml_system['name']
+              system.id = yaml_system['id'] if yaml_system['id']
+              system.description = yaml_system['description'] if yaml_system['description']
               
-              connection = yaml_system['connection']
-              if connection
-                ip_address, ip_port = connection.split(':')
-                system.ip_address = ip_address
-                system.ip_port = ip_port if ip_port
-              end
-              
+              parse_connection_node system, yaml_system['connection']
               project << system
             end
           end
@@ -44,6 +38,38 @@ module NetLinx
       # @return {NetLinx::Workspace}
       def self.parse_file file
         parse File.open(file, 'r').read
+      end
+      
+      private
+      
+      def self.parse_connection_node system, connection_node
+        case connection_node
+        when String
+          if connection_node =~ /\Acom\d+(?::|\z)/i
+            # Serial Connection
+            com_port, baud_rate = connection_node.split ':'
+            system.com_port = com_port.downcase.to_sym
+            system.baud_rate = baud_rate.to_i if baud_rate
+          else
+            # IP Connection
+            ip_address, ip_port = connection_node.split ':'
+            system.ip_address = ip_address
+            system.ip_port = ip_port.to_i if ip_port
+          end
+        when Hash
+          if connection_node['host']
+            # IP Connection
+            system.ip_address = connection_node['host']
+            system.ip_port = connection_node['port'] if connection_node['port']
+          else
+            # Serial Connection
+            system.com_port = connection_node['port'].downcase.to_sym
+            system.baud_rate = connection_node['baud_rate'] if connection_node['baud_rate']
+            system.data_bits = connection_node['data_bits'] if connection_node['data_bits']
+            system.stop_bits = connection_node['stop_bits'] if connection_node['stop_bits']
+            system.parity = connection_node['parity'].downcase.to_sym if connection_node['parity']
+          end
+        end
       end
       
     end
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -19,14 +19,13 @@ describe NetLinx::Workspace::YAML do
       project.systems.first
     end
     
+    before { workspace.should be_a NetLinx::Workspace }
+    
     describe "single system" do
       let(:file) { 'single_system.yaml' }
-      
       let(:name) { 'Client - Project' }
       
       specify do
-        workspace.should be_a NetLinx::Workspace
-        
         workspace.name.should eq name
         workspace.description.should eq ''
         
@@ -39,16 +38,79 @@ describe NetLinx::Workspace::YAML do
         system.ip_address.should eq '192.168.1.2'
         system.ip_port.should eq 1319
       end
+      
+      specify "test included files"
     end
     
     describe "multiple systems" do
       let(:file) { 'multiple_systems.yaml' }
-      specify
+      let(:name) { 'Production' }
+      
+      specify do
+        workspace.name.should eq name
+        workspace.description.should eq ''
+        
+        workspace.projects.count.should eq 1
+        project.name.should eq name
+        project.description.should eq ''
+        
+        project.systems[0].tap do |system|
+          system.name.should eq name
+          system.id.should eq 0
+          system.description.should eq ''
+          system.ip_address.should eq '192.168.1.2'
+          system.ip_port.should eq 1319
+        end
+        
+        project.systems[1].tap do |system|
+          system.name.should eq 'Test Suite'
+          system.id.should eq 2
+          system.description.should eq 'For testing code.'
+          system.ip_address.should eq '192.168.253.2'
+          system.ip_port.should eq 5000
+        end
+        
+        project.systems[2].tap do |system|
+          system.name.should eq 'Serial Connection 1'
+          system.id.should eq 0
+          system.description.should eq ''
+          system.com_port.should eq :com5
+          system.baud_rate.should eq 38400
+          system.data_bits.should eq 8
+          system.parity.should eq :none
+          system.stop_bits.should eq 1
+        end
+        
+        project.systems[3].tap do |system|
+          system.name.should eq 'Serial Connection 2'
+          system.id.should eq 0
+          system.description.should eq ''
+          system.com_port.should eq :com2
+          system.baud_rate.should eq 57600
+          system.data_bits.should eq 8
+          system.parity.should eq :none
+          system.stop_bits.should eq 1
+        end
+        
+        project.systems[4].tap do |system|
+          system.name.should eq 'Serial Connection 3'
+          system.id.should eq 0
+          system.description.should eq ''
+          system.com_port.should eq :com3
+          system.baud_rate.should eq 115200
+          system.data_bits.should eq 7
+          system.parity.should eq :even
+          system.stop_bits.should eq 2
+        end
+      end
+      
+      specify "test included files"
     end
     
     describe "workspace" do
       let(:file) { 'workspace.yaml' }
       specify
+      specify "test included files"
     end
   end
   
