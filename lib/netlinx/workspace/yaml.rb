c49066f494aa82e6cfb53ea294aa96f18eae102f
Alex McLain <alex@alexmclain.com>
2015-02-18 10:52:13 -0800

    Implemented yaml parsing of single system.

--- lib/netlinx/workspace/yaml.rb
+++ lib/netlinx/workspace/yaml.rb
@@ -7,10 +7,43 @@ module NetLinx
       
       # @return {NetLinx::Workspace}
       def self.parse string
+        yaml = ::YAML.load string
+        yaml_systems = yaml['systems']
+        
+        workspace = NetLinx::Workspace.new
+        
+        if yaml_systems
+          # Load systems into an implicit project and workspace.
+          workspace.name = yaml_systems.first['name']
+          project = NetLinx::Project.new name: yaml_systems.first['name']
+          workspace << project
+          
+          yaml_systems.each do |yaml_system|
+            NetLinx::System.new.tap do |system|
+              system.name = yaml_system['name']
+              system.id = yaml_system['id']
+              system.description = yaml_system['description']
+              
+              connection = yaml_system['connection']
+              if connection
+                ip_address, ip_port = connection.split(':')
+                system.ip_address = ip_address
+                system.ip_port = ip_port if ip_port
+              end
+              
+              project << system
+            end
+          end
+        else
+          # An explicit workspace is defined.
+        end
+        
+        workspace
       end
       
       # @return {NetLinx::Workspace}
       def self.parse_file file
+        parse File.open(file, 'r').read
       end
       
     end
--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -1,5 +1,55 @@
 require 'netlinx/workspace/yaml'
 
 describe NetLinx::Workspace::YAML do
-  specify
+  
+  subject { NetLinx::Workspace::YAML }
+  
+  it { should respond_to :parse }
+  it { should respond_to :parse_file }
+  
+  describe "parse" do
+    subject { NetLinx::Workspace::YAML.parse_file "spec/workspace/yaml/config/#{file}" }
+    let(:workspace) { subject }
+    
+    def project
+      workspace.projects.first
+    end
+    
+    def system
+      project.systems.first
+    end
+    
+    describe "single system" do
+      let(:file) { 'single_system.yaml' }
+      
+      let(:name) { 'Client - Project' }
+      
+      specify do
+        workspace.should be_a NetLinx::Workspace
+        
+        workspace.name.should eq name
+        workspace.description.should eq ''
+        
+        workspace.projects.count.should eq 1
+        project.name.should eq name
+        project.description.should eq ''
+        
+        system.id.should eq 3
+        system.description.should eq 'System description.'
+        system.ip_address.should eq '192.168.1.2'
+        system.ip_port.should eq 1319
+      end
+    end
+    
+    describe "multiple systems" do
+      let(:file) { 'multiple_systems.yaml' }
+      specify
+    end
+    
+    describe "workspace" do
+      let(:file) { 'workspace.yaml' }
+      specify
+    end
+  end
+  
 end
