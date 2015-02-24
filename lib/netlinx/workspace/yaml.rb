f81b6f8b0540a57b3861c9a2996a7addaebc6bc1
Alex McLain <alex@alexmclain.com>
2015-02-23 16:51:34 -0800

    Refactored yaml system parser to allow for workspace defintion.

--- lib/netlinx/workspace/yaml.rb
+++ lib/netlinx/workspace/yaml.rb
@@ -18,92 +18,7 @@ module NetLinx
           project = NetLinx::Project.new name: yaml_systems.first['name']
           workspace << project
           
-          yaml_systems.each do |yaml_system|
-            project << NetLinx::System.new.tap do |system|
-              system.name = yaml_system['name'] if yaml_system['name']
-              system.active = yaml_system['active'] || false
-              system.id = yaml_system['id'] if yaml_system['id']
-              system.description = yaml_system['description'] if yaml_system['description']
-              
-              root = yaml_system['root'] # File system root directory.
-              
-              parse_connection_node system, yaml_system['connection']
-              
-              # Auto-include master source file.
-              master_src_path = yaml_system['axs'] || "#{system.name}.axs"
-              master_src_path = "#{root}/#{master_src_path}" if root
-              add_file_to_system system, master_src_path, :master
-              
-              yaml_excluded_files = yaml_system['excluded_files']
-              
-              # Auto-include 'include' directory.
-              include_path = 'include/**/*.axi'
-              include_path = "#{root}/#{include_path}" if root
-              include_files = Dir[include_path]
-              include_files -= Dir[*yaml_excluded_files] if yaml_excluded_files
-              include_files.each do |file_path|
-                add_file_to_system system, file_path, :include
-              end
-              
-              # Additional include paths.
-              yaml_files = yaml_system['includes']
-              if yaml_files
-                files = Dir[*yaml_files]
-                files -= Dir[*yaml_excluded_files] if yaml_excluded_files
-                
-                files.each do |file_path|
-                  add_file_to_system system, file_path, :include
-                end
-              end
-              
-              # Auto-include 'module' directory.
-              module_path = 'module/**/*.{tko,jar}'
-              module_path = "#{root}/#{module_path}" if root
-              module_files = Dir[module_path]
-              module_files -= Dir[*yaml_excluded_files] if yaml_excluded_files
-              module_files.each do |file_path|
-                add_file_to_system system, file_path, 
-                  (File.extname(file_path) == '.jar') ?
-                    :duet :
-                    File.extname(file_path)[1..-1].downcase.to_sym
-              end
-              
-              # Additional module paths.
-              yaml_files = yaml_system['modules']
-              if yaml_files
-                files = Dir[*yaml_files]
-                files -= Dir[*yaml_excluded_files] if yaml_excluded_files
-                
-                files.each do |file_path|
-                  add_file_to_system system, file_path, File.extname(file_path)[1..-1].downcase.to_sym
-                end
-              end
-              
-              # Touch panel files.
-              yaml_touch_panels = yaml_system['touch_panels']
-              if yaml_touch_panels
-                yaml_touch_panels.each do |yaml_touch_panel|
-                  file_path = "touch_panel/#{yaml_touch_panel['path']}"
-                  file_path = "#{root}/#{file_path}" if root
-                  add_file_to_system(system, file_path, File.extname(file_path)[1..-1].downcase.to_sym)
-                    .tap { |file| attach_devices file, yaml_touch_panel }
-                end
-              end
-              
-              # IR files.
-              yaml_ir_files = yaml_system['ir']
-              if yaml_ir_files
-                yaml_ir_files.each do |yaml_ir|
-                  file_path = "ir/#{yaml_ir['path']}"
-                  file_path = "#{root}/#{file_path}" if root
-                  add_file_to_system(system, file_path, :ir)
-                    .tap { |file| attach_devices file, yaml_ir }
-                end
-              end
-              
-              # TODO: Modules.
-            end
-          end
+          parse_systems project, yaml_systems
         else
           # An explicit workspace is defined.
         end
@@ -186,6 +101,93 @@ module NetLinx
             system.data_bits = connection_node['data_bits'] if connection_node['data_bits']
             system.stop_bits = connection_node['stop_bits'] if connection_node['stop_bits']
             system.parity = connection_node['parity'].downcase.to_sym if connection_node['parity']
+          end
+        end
+      end
+      
+      def self.parse_systems project, yaml_systems
+        yaml_systems.each do |yaml_system|
+          project << NetLinx::System.new.tap do |system|
+            system.name = yaml_system['name'] if yaml_system['name']
+            system.active = yaml_system['active'] || false
+            system.id = yaml_system['id'] if yaml_system['id']
+            system.description = yaml_system['description'] if yaml_system['description']
+            
+            root = yaml_system['root'] # File system root directory.
+            
+            parse_connection_node system, yaml_system['connection']
+            
+            # Auto-include master source file.
+            master_src_path = yaml_system['axs'] || "#{system.name}.axs"
+            master_src_path = "#{root}/#{master_src_path}" if root
+            add_file_to_system system, master_src_path, :master
+            
+            yaml_excluded_files = yaml_system['excluded_files']
+            
+            # Auto-include 'include' directory.
+            include_path = 'include/**/*.axi'
+            include_path = "#{root}/#{include_path}" if root
+            include_files = Dir[include_path]
+            include_files -= Dir[*yaml_excluded_files] if yaml_excluded_files
+            include_files.each do |file_path|
+              add_file_to_system system, file_path, :include
+            end
+            
+            # Additional include paths.
+            yaml_files = yaml_system['includes']
+            if yaml_files
+              files = Dir[*yaml_files]
+              files -= Dir[*yaml_excluded_files] if yaml_excluded_files
+              
+              files.each do |file_path|
+                add_file_to_system system, file_path, :include
+              end
+            end
+            
+            # Auto-include 'module' directory.
+            module_path = 'module/**/*.{tko,jar}'
+            module_path = "#{root}/#{module_path}" if root
+            module_files = Dir[module_path]
+            module_files -= Dir[*yaml_excluded_files] if yaml_excluded_files
+            module_files.each do |file_path|
+              add_file_to_system system, file_path, 
+                (File.extname(file_path) == '.jar') ?
+                  :duet :
+                  File.extname(file_path)[1..-1].downcase.to_sym
+            end
+            
+            # Additional module paths.
+            yaml_files = yaml_system['modules']
+            if yaml_files
+              files = Dir[*yaml_files]
+              files -= Dir[*yaml_excluded_files] if yaml_excluded_files
+              
+              files.each do |file_path|
+                add_file_to_system system, file_path, File.extname(file_path)[1..-1].downcase.to_sym
+              end
+            end
+            
+            # Touch panel files.
+            yaml_touch_panels = yaml_system['touch_panels']
+            if yaml_touch_panels
+              yaml_touch_panels.each do |yaml_touch_panel|
+                file_path = "touch_panel/#{yaml_touch_panel['path']}"
+                file_path = "#{root}/#{file_path}" if root
+                add_file_to_system(system, file_path, File.extname(file_path)[1..-1].downcase.to_sym)
+                  .tap { |file| attach_devices file, yaml_touch_panel }
+              end
+            end
+            
+            # IR files.
+            yaml_ir_files = yaml_system['ir']
+            if yaml_ir_files
+              yaml_ir_files.each do |yaml_ir|
+                file_path = "ir/#{yaml_ir['path']}"
+                file_path = "#{root}/#{file_path}" if root
+                add_file_to_system(system, file_path, :ir)
+                  .tap { |file| attach_devices file, yaml_ir }
+              end
+            end
           end
         end
       end
